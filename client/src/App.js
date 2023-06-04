c618436da91b51f4e5f2de300bb7e2c21591fed5
Ankitbagga75 <ankitbagga7572@gmail.com>
2023-06-04 23:30:03 +0530

    Second Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  );
+import React from 'react'
+import {createBrowserRouter,RouterProvider } from 'react-router-dom';
+export default function App() {
+    return (
+        <div>
+           <h1 className="text-3xl font-bold underline">
+            Hello World!
+           </h1>
+        </div>
+    )
 }
-
-export default App;
