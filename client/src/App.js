4e6fbde8473143d792c1c82aaef91c2b7b2a794c
Ankitbagga75 <ankitbagga7572@gmail.com>
2023-06-05 23:46:47 +0530

    Started the username page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,47 @@
 import React from 'react'
 import {createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Password from './components/Password';
+import Reset from './components/Reset';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import PageNotFound from './components/PageNotFound';
+import Register from './components/Register';
+import Username from './components/Username';
+
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <Username></Username>
+    },
+    {
+        path: '/register',
+        element : <Register></Register>
+    },
+    {
+        path: '/password',
+        element : <Password></Password>
+    },
+    {
+        path: '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path: '/profile',
+        element : <Profile></Profile>
+    },
+    {
+        path: '/recovery',
+        element : <Recovery></Recovery>
+    },
+    {
+        path: '/*',
+        element : <PageNotFound></PageNotFound>
+    }
+])
 export default function App() {
     return (
-        <div>
-           <h1 className="text-3xl font-bold underline">
-            Hello World!
-           </h1>
-        </div>
+        <main>
+            <RouterProvider router ={router}></RouterProvider>
+        </main>
     )
 }
